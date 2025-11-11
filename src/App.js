e225b137fcd78aee68b27a3a889a56ba0aeaf6eb
Victor Mussoline <victor8augusto010604@hotmail.com>
2025-11-11 11:19:13 -0300

    beta 1.2
    Inserção de formulário, mas a logo ainda não está correta

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import {db} from './firebase.js'
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 function App() {
+
+  const [user, setUser] = useState(null);
 
   useEffect(()=>{
     console.log(db)
@@ -12,6 +14,30 @@ function App() {
   
   return (
     <div className="App">
+      <div className="header">
+    <div className="center">
+        <div className="header_logo">
+          <a href=""><img src="https://ignicaodigital.com.br/wp-content/uploads/2016/10/instagram-1594387_640.png"/></a>
+        </div>
+
+        {
+        (user)?
+        <div>Olá Victor</div>
+        :
+        <div className="header_loginForm">
+          <form>
+            <input type="text" placeholder="Login"  />
+            <input type="password" placeholder="Senha"  />
+            <input type="submit" name="action" value="Logar!" />
+          </form>
+        </div>
+
+        }
+
+
+
+      </div>
+    </div>
 
     </div>
   );
